2d366932e8b0d9add676acf2b3f0551c718761cc
Sahil <garg07825@gmail.com>
2023-12-15 05:12:42 +0530

    Created a sticky notes component

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,39 @@
+import React, { useState } from "react";
+import StickyNotes from "./Components/StickyNotes";
 function App() {
+  const [notes, setNotes] = useState([]);
+
+  const addNewNote = () => {
+    setNotes([
+      ...notes,
+      { id: notes.length + 1, content: "", position: { top: 100, left: 200 } },
+    ]);
+  };
+
+  const handleContentChange = (id, newContent) => {
+    setNotes((prevNotes) =>
+      prevNotes.map((note) =>
+        note.id === id ? { ...note, content: newContent } : note
+      )
+    );
+  };
+
   return (
-    <>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
-    </>
+    <div>
+      <button onClick={addNewNote} className="add-note-button">
+        Add New Note
+      </button>
+      {notes.map((note) => (
+        <StickyNotes
+          key={note.id}
+          content={note.content}
+          onContentChange={(newContent) =>
+            handleContentChange(note.id, newContent)
+          }
+          position={note.position}
+        />
+      ))}
+    </div>
   );
 }
 
