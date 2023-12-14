534cbf185202b166c7f9a5d71b2140389ec350d4
Sahil <garg07825@gmail.com>
2023-12-15 05:19:42 +0530

    now we can write the content

--- src/Components/StickyNotes.jsx
+++ src/Components/StickyNotes.jsx
@@ -1,3 +1,23 @@
-export default function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
-}
+import React from "react";
+
+const StickyNotes = ({ content, onContentChange, position, style }) => {
+  return (
+    <div
+      className={`sticky-note rounded-lg p-4 bg-gray-100 shadow-md w-64 h-64 overflow-hidden ${style}`}
+      style={{
+        position: "absolute",
+        top: position.top,
+        left: position.left,
+      }}
+    >
+      <textarea
+        className="w-full h-full resize-none outline-none border-none focus:ring-1 focus:ring-blue-500"
+        value={content}
+        onChange={(e) => onContentChange(e.target.value)}
+        placeholder="Write your note..."
+      />
+    </div>
+  );
+};
+
+export default StickyNotes;
